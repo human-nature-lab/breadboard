9e4afde4b44e7682ac81160fa61bda71485b10a1
wyattis <wyattisrael@gmail.com>
2017-12-19 16:06:46 -0500

    removed hot module swapping:

--- frontend/webpack/webpack.dev.js
+++ frontend/webpack/webpack.dev.js
@@ -20,6 +20,6 @@ config.plugins = [
 const clientConfig = Object.assign({}, config, require('./webpack.client.js'));
 const designConfig = Object.assign({}, config, require('./webpack.design.js'));
 module.exports = [clientConfig, designConfig].map(c => {
-    c.entry = ['webpack/hot/dev-server', 'webpack-dev-server/client?http://localhost:8080'].concat(c.entry);
+    // c.entry = ['webpack/hot/dev-server', 'webpack-dev-server/client?http://localhost:8080'].concat(c.entry);
     return c;
 });
