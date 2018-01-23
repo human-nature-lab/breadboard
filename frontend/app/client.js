42755bfbe99cbc74aaf169890a9cb728f342586e
wyattis <wyattisrael@gmail.com>
2018-01-23 10:06:47 -0500

    Override breadboard.client controller,service,directive methods to allow dynamic loading after angular has bootstrapped

--- frontend/app/client.js
+++ frontend/app/client.js
@@ -49,3 +49,47 @@ angular.module('breadboard.client', [
       controller: ClientCtrl,
     }
   }]);
+var app = angular.module('breadboard.client');
+
+app.config(
+  function( $controllerProvider, $provide, $compileProvider ) {
+    // Since the "shorthand" methods for component
+    // definitions are no longer valid, we can just
+    // override them to use the providers for post-
+    // bootstrap loading.
+    // Let's keep the older references.
+    app._controller = app.controller;
+    app._service = app.service;
+    app._factory = app.factory;
+    app._value = app.value;
+    app._directive = app.directive;
+    // Provider-based controller.
+    app.controller = function( name, constructor ) {
+      $controllerProvider.register( name, constructor );
+      return( this );
+    };
+    // Provider-based service.
+    app.service = function( name, constructor ) {
+      $provide.service( name, constructor );
+      return( this );
+    };
+    // Provider-based factory.
+    app.factory = function( name, factory ) {
+      $provide.factory( name, factory );
+      return( this );
+    };
+    // Provider-based value.
+    app.value = function( name, value ) {
+      $provide.value( name, value );
+      return( this );
+    };
+    // Provider-based directive.
+    app.directive = function( name, factory ) {
+      $compileProvider.directive( name, factory );
+      return( this );
+    };
+    // NOTE: You can do the same thing with the "filter"
+    // and the "$filterProvider"; but, I don't really use
+    // custom filters.
+  }
+);
