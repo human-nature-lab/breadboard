533a51a57642fe4ffd57f65eeea2711b33ddc0b3
wyattis <wyattisrael@gmail.com>
2020-02-24 10:33:58 -0500

    Removed 'synchronized' blocks from EventBus and changed synchronization method to take advantage of multithreaded environment.

--- app/models/EventBus.java
+++ app/models/EventBus.java
@@ -2,15 +2,14 @@ package models;
 
 import play.Logger;
 import groovy.lang.Closure;
-import java.lang.*;
 import java.util.Map;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.Collections;
 
 class EventHandler<A> {
-  public List<Closure<A>> closures = Collections.synchronizedList(new ArrayList());
+  public List<Closure<A>> closures = new CopyOnWriteArrayList<>();
 
   public void addClosure (Closure<A> closure) {
     this.closures.add(closure);
@@ -21,10 +20,8 @@ class EventHandler<A> {
   }
 
   public void emit (A... data) {
-    synchronized (this.closures) {
-      for (Closure<A> closure : this.closures) {
-        closure.call(data);
-      }
+    for (Closure<A> closure : this.closures) {
+      closure.call(data);
     }
   }
 
@@ -36,70 +33,65 @@ class EventHandler<A> {
 
 public class EventBus <T> {
 
-  private Map<String, EventHandler<T>> events = Collections.synchronizedMap(new HashMap());
+  private Map<String, EventHandler<T>> events = new ConcurrentHashMap();
 
   public EventHandler<T> register (String eventName) {
-    synchronized (this.events) {
-      EventHandler<T> handler = new EventHandler();
-      this.events.put(eventName, handler);
-      return handler;
-    }
+    EventHandler<T> handler = new EventHandler();
+    this.events.put(eventName, handler);
+    return handler;
   }
 
   public void unregister (String eventName) {
-    synchronized (this.events) {
-      this.events.remove(eventName);
-    }
+    this.events.remove(eventName);
   }
 
   public void on (String eventName, Closure closure) {
-    synchronized (this.events) {
-      EventHandler<T> event = this.events.get(eventName);
-      // Automatically register the event if it hasn't been registered yet
-      if (event == null) {
-        event = register(eventName);
-      }
-      event.addClosure(closure);
-      this.logThread("on"); 
+    EventHandler<T> event = this.events.get(eventName);
+    // Automatically register the event if it hasn't been registered yet
+    if (event == null) {
+      event = register(eventName);
     }
+    event.addClosure(closure);
+    this.logThread("on", eventName); 
   }
 
   public void off (String eventName) {
-    synchronized (this.events) {
-      this.events.remove(eventName);
-      this.logThread("off 1");
-    }
+    this.events.remove(eventName);
+    this.logThread("off 1", eventName);
   }
   
   public void off (String eventName, Closure closure) {
-    synchronized (this.events) {
-      EventHandler<T> event = this.events.get(eventName);
-      if (event != null) {
-        event.removeClosure(closure);
-      }
-      this.logThread("off 2");
+    EventHandler<T> event = this.events.get(eventName);
+    if (event != null) {
+      event.removeClosure(closure);
     }
+    this.logThread("off 2", eventName);
   }
 
   public void emit (String eventName, T... payload) {
-    synchronized (this.events) {
-      EventHandler<T> event = this.events.get(eventName);
-      if (event != null) {
-        event.emit(payload);
-      }
-      this.logThread("emit");
+    EventHandler<T> event = this.events.get(eventName);
+    if (event != null) {
+      event.emit(payload);
     }
+    this.logThread("emit", eventName);
   }
 
-  private void logThread (String name) {
-    // Logger.debug(name + " thread " + Thread.currentThread().getId() + " size " + this.events.keySet().size());
+  private void logThread (String methodName, String eventName) {
+    EventHandler<T> event = this.events.get(eventName);
+    String msg = methodName + " thread " + Thread.currentThread().getId() + " size " + this.events.keySet().size() + " " + eventName;
+    if (event != null) {
+      msg += " listeners " + event.closures.size();
+    }
+    Logger.trace(msg);
+  }
+
+  private void logThread (String methodName) {
+    Logger.trace(methodName + " thread " + Thread.currentThread().getId() + " size " + this.events.keySet().size());
   }
 
   public void clear () {
-    synchronized (this.events) {
-      this.events.clear();
-      this.logThread("clear");
-    }
+    this.events.clear();
+    this.logThread("clear");
   }
 
 }
