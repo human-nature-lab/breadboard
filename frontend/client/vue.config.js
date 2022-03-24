d714967335fc9647114938a89c502b5310784653
wyattis <wyattisrael@gmail.com>
2022-03-24 12:58:39 -0600

    change ts target

--- frontend/client/vue.config.js
+++ frontend/client/vue.config.js
@@ -3,9 +3,9 @@ module.exports = {
     // config.module.rule('ts').use('ts-loader').options({ transpileOnly: true, appendTsSuffixTo: [/\.vue$/] })
   },
   configureWebpack: {
-    // output: {
-    //   libraryExport: 'default'
-    // }
+    output: {
+      libraryExport: 'default'
+    }
   },
   transpileDependencies: [],
 }
