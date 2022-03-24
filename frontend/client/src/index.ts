3af934d700efb954919fd8eb84ae096173ae750a
wyattis <wyattisrael@gmail.com>
2022-03-24 16:00:43 -0600

    minor changes to import correctly

--- frontend/client/src/index.ts
+++ frontend/client/src/index.ts
@@ -1,7 +1,19 @@
-export * as Choice from './components/Choice.vue'
-export * as ConnectionStatus from './components/ConnectionStatus.vue'
-export * as PlayerChoices from './components/PlayerChoices.vue'
-export * as PlayerText from './components/PlayerText.vue'
-export * as SVGGraph from './components/SVGGraph.vue'
-export * as Timer from './components/Timer.vue'
-export * as DefaultView from './mixins/DefaultView'
+import Choice from './components/Choice.vue'
+import ConnectionStatus from './components/ConnectionStatus.vue'
+import PlayerChoices from './components/PlayerChoices.vue'
+import PlayerTimers from './components/PlayerTimers.vue'
+import PlayerText from './components/PlayerText.vue'
+import SVGGraph from './components/SVGGraph.vue'
+import Timer from './components/Timer.vue'
+import DefaultView from './mixins/DefaultView'
+
+export {
+  Choice,
+  ConnectionStatus,
+  PlayerChoices,
+  PlayerTimers,
+  PlayerText,
+  SVGGraph,
+  Timer,
+  DefaultView,
+}
