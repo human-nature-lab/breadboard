1e157b7d633f2135412bb78a00f48aa52228bc21
wyattis <wyattisrael@gmail.com>
2024-07-03 10:58:17 -0400

    fix file permissions when executing breadboard

--- breadboard-v2.3.1.sh
+++ breadboard-v2.3.1.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+chmod u+x ./bin/breadboard
+chmod u+x ./db/breadboard.h2.db
 ./bin/breadboard -Dhttps.port=9443 -Dapplication.secret="pleasechangethis" -Dconfig.file=conf/application-prod.conf -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC
