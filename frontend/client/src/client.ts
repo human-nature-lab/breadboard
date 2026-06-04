1c874a8d40eb1968aba470f3c3e74db134396675
wyattis <wyattisrael@gmail.com>
2026-06-04 17:42:17 -0400

    fix some bad formatting

--- frontend/client/src/client.ts
+++ frontend/client/src/client.ts
@@ -3,46 +3,46 @@ import {
   BreadboardConfig,
   BreadboardClass,
   VueLoadOpts,
-} from "@human-nature-lab/breadboard-core";
-import DefaultView from "./mixins/DefaultView";
-import "./client.sass";
-window.Breadboard = Breadboard;
+} from '@human-nature-lab/breadboard-core'
+import DefaultView from './mixins/DefaultView'
+import './client.sass'
+window.Breadboard = Breadboard
 
 async function client() {
-  let config: BreadboardConfig;
+  let config: BreadboardConfig
   try {
-    config = await Breadboard.loadConfig();
+    config = await Breadboard.loadConfig()
   } catch (err) {
-    console.error("Breadboard: Unable to load Breadboard");
-    throw err;
+    console.error('Breadboard: Unable to load Breadboard')
+    throw err
   }
 
   try {
-    window.loadVue = loadVue;
-    window.createDefaultVue = createDefaultVue;
-    window.loadAngularClient = loadAngularClient;
-    window.loadModules = loadModules;
-    await Breadboard.addScriptFromString(config.clientGraph);
+    window.loadVue = loadVue
+    window.createDefaultVue = createDefaultVue
+    window.loadAngularClient = loadAngularClient
+    window.loadModules = loadModules
+    await Breadboard.addScriptFromString(config.clientGraph)
   } catch (err) {
-    console.error("Breadboard: Unable to run client-graph.js");
-    throw err;
+    console.error('Breadboard: Unable to run client-graph.js')
+    throw err
   }
 }
 
-client();
+client()
 
 // Load Vue and optional dependencies
 export function loadVue(opts: VueLoadOpts) {
   opts = Object.assign(
     {
-      vueVersion: "2.6.11",
-      vuetifyVersion: "2.3.7",
-      mdiVersion: "5.4.55",
+      vueVersion: '2.6.11',
+      vuetifyVersion: '2.3.7',
+      mdiVersion: '5.4.55',
       useDev: false,
       withVuetify: true,
     },
     opts,
-  );
+  )
 
   return async function (core: BreadboardClass, config: BreadboardConfig) {
     // this.addStyleFromURL('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900')
@@ -53,16 +53,16 @@ export function loadVue(opts: VueLoadOpts) {
       // this.addStyleFromURL(`https://cdn.jsdelivr.net/npm/vuetify@${opts.vuetifyVersion}/dist/vuetify.min.css`)
     }
     //@ts-ignore
-    window.Vue = (await import("vue")).default;
-    window.Vuetify = (await import("vuetify")).default;
-    await import(/* webpackChunkName: "vue-components" */ "./vue-components");
+    window.Vue = (await import('vue')).default
+    window.Vuetify = (await import('vuetify')).default
+    await import(/* webpackChunkName: "vue-components" */ './vue-components')
     // await this.addScriptFromURL(`${config.assetsRoot}/bundles/vue-components.js`)
     // await import('./vue-components')
     // core.addStyleFromURL(`${config.assetsRoot}/bundles/vue-components.css`)
     // await this.addScriptFromURL(`https://cdnjs.cloudflare.com/ajax/libs/vue/${opts.vueVersion}/vue.${opts.useDev ? 'common.dev.' : 'min.'}js`)
     // Register Vuetify components
-    window.Vue.use(window.Vuetify);
-  };
+    window.Vue.use(window.Vuetify)
+  }
 }
 
 export function loadModules(...names: string[]) {
@@ -84,7 +84,7 @@ export function loadModules(...names: string[]) {
     //   // mods.push(this.addStyleFromURL(`${config.assetsRoot}/bundles/modules/${name}.css`).catch(err => console.log(err)))
     // }
     // await Promise.all(mods)
-  };
+  }
 }
 
 /**
@@ -92,22 +92,22 @@ export function loadModules(...names: string[]) {
  * @param template
  */
 export async function createDefaultVue(template: string, mixin?: object) {
-  const Vue = window.Vue;
-  const Vuetify = window.Vuetify;
-  const mixins = [DefaultView];
+  const Vue = window.Vue
+  const Vuetify = window.Vuetify
+  const mixins = [DefaultView]
   if (mixin) {
     // @ts-ignore
-    mixins.push(mixin);
+    mixins.push(mixin)
   }
   return new Vue({
     vuetify: new Vuetify({
       icons: {
-        iconfont: "mdi",
+        iconfont: 'mdi',
       },
     }),
     mixins: mixins,
     template: template,
-  }).$mount("#app");
+  }).$mount('#app')
 }
 
 /**
@@ -115,42 +115,42 @@ export async function createDefaultVue(template: string, mixin?: object) {
  */
 export function loadAngularClient() {
   return async function (core: BreadboardClass, config: BreadboardConfig) {
-    core.addStyleFromURL(`${config.assetsRoot}/bundles/client.css`);
-    core.addStyleFromURL(`${config.assetsRoot}/bundles/client-angular.css`);
+    core.addStyleFromURL(`${config.assetsRoot}/bundles/client.css`)
+    core.addStyleFromURL(`${config.assetsRoot}/bundles/client-angular.css`)
     core.addStyleFromURL(
-      "https://fonts.googleapis.com/css?family=Open+Sans:700,400",
-    );
-    core.addStyleFromURL(`${config.assetsRoot}/css/bootstrap.min.css`);
+      'https://fonts.googleapis.com/css?family=Open+Sans:700,400',
+    )
+    core.addStyleFromURL(`${config.assetsRoot}/css/bootstrap.min.css`)
     core.addStyleFromURL(
       `${config.assetsRoot}/css/font-awesome-4.7.0/css/font-awesome.min.css`,
-    );
+    )
     await Promise.all([
       core.addScriptFromURL(
-        "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js",
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js',
       ),
       core.addScriptFromURL(`${config.assetsRoot}/bundles/client-angular.js`),
-    ]);
+    ])
     await Promise.all([
       core.addScriptFromURL(
-        "https://cdnjs.cloudflare.com/ajax/libs/d3/2.10.0/d3.v2.js",
+        'https://cdnjs.cloudflare.com/ajax/libs/d3/2.10.0/d3.v2.js',
       ),
       core.addScriptFromURL(
-        "https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.12.1/ui-bootstrap-tpls.js",
+        'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.12.1/ui-bootstrap-tpls.js',
       ),
-    ]);
+    ])
     // @ts-ignore
-    const ang = angular;
-    const init = window.bbClientInit;
+    const ang = angular
+    const init = window.bbClientInit
     ang.element(document).ready(function () {
-      const t = document.createElement("div");
-      const app = document.createElement("app");
-      app.innerText = "Loading...";
-      document.body.setAttribute("ng-app", "breadboard.client");
-      const c = document.getElementById("app");
-      if (!c) throw new Error("Unable to initialize app");
-      document.body.replaceChild(app, c);
-      init();
-      ang.bootstrap(document, ["breadboard.client"]);
-    });
-  };
+      const t = document.createElement('div')
+      const app = document.createElement('app')
+      app.innerText = 'Loading...'
+      document.body.setAttribute('ng-app', 'breadboard.client')
+      const c = document.getElementById('app')
+      if (!c) throw new Error('Unable to initialize app')
+      document.body.replaceChild(app, c)
+      init()
+      ang.bootstrap(document, ['breadboard.client'])
+    })
+  }
 }
